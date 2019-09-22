b7893ac2b0386c59e46a9cc79eb40df1cace2fbd
huangdongmei <849013525@qq.com>
2019-09-22 23:35:48 +0800

    9.22

--- src/dailyCoding/convertTreeToList.java
+++ src/dailyCoding/convertTreeToList.java
@@ -1,5 +1,51 @@
 package dailyCoding;
 
-public class convertTreeToList {
+import java.util.ArrayList;
 
+public class convertTreeToList {
+	
+	 ArrayList<TreeNode> arrayList = new ArrayList<>();
+	 
+	 public TreeNode Convert(TreeNode pRootOfTree) {
+	        if(pRootOfTree==null)
+	        	return pRootOfTree;
+	        
+	        addNode(pRootOfTree);
+	        
+	        return convertPointer(arrayList);       
+	        
+	    }
+	 
+	 //中序遍历
+	 public void addNode(TreeNode root) {
+		 if(root.left!=null)
+			 addNode(root.left);;
+		 
+		 arrayList.add(root);
+		 
+		 if(root.right!=null) {
+			 addNode(root.right);
+		 } 
+	 }
+	 
+	 //修改指针
+	 public TreeNode convertPointer(ArrayList<TreeNode> arrayList) {
+		 for(int i=0;i<arrayList.size()-1;i++) {
+			 arrayList.get(i).right=arrayList.get(i+1);
+			 arrayList.get(i+1).left=arrayList.get(i);
+		 }
+		 
+		 
+		 return arrayList.get(0);
+	 }
+}
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
 }
--- src/dailyCoding/moreThanHalfList.java
+++ src/dailyCoding/moreThanHalfList.java
@@ -1,5 +1,26 @@
 package dailyCoding;
 
-public class moreThanHalfList {
+import java.util.HashMap;
+import java.util.Iterator;
 
+public class moreThanHalfList {
+	 public int MoreThanHalfNum_Solution(int [] array) {
+	        HashMap<Integer,Integer> map = new HashMap<>();
+	        
+	        for(int i :array) {
+	        	if(map.get(i)!=null)
+	        	   map.put(i, map.get(i)+1);
+	        	else
+	        		map.put(i, 1);
+	        }
+	        
+	        for(int key : map.keySet()) {
+	        	if(map.get(key) > (array.length/2)) {
+	        		return key;
+	        	}	
+	        }
+	        
+	        return 0;
+	    
+      }
 }
