50922579d654ca19b6649aa4a7e278ede124a4ea
huangdongmei <849013525@qq.com>
2019-10-06 23:48:36 +0800

    用字符串实现循环左移

--- src/dailyCoding/LeftRotateString.java
+++ src/dailyCoding/LeftRotateString.java
@@ -1,6 +1,17 @@
 package dailyCoding;
 
+/**
+ * 用字符串实现循环左移
+ * @author h
+ *2019.10.06
+ */
 public class LeftRotateString {
-	
-
+    public String LeftRotateString(String str,int n) {
+        if(str.length()==0 || n==0)
+        	return str;
+        
+        n=n%str.length();
+        
+        return str.substring(n,str.length()) + str.substring(0,n);
+    }
 }
